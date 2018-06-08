b47d19fff99b0701619de0ea3dc8d884d6d86f49
Aymeric TOULOUSE <atoulous@e3r4p4.42.fr>
2018-06-08 19:28:42 +0200

    fri8jun - run eject, add all config files

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import logo from '../../logo.svg';
-import './Home.css';
+import './Home.scss';
 
 class Home extends Component {
   state = {
