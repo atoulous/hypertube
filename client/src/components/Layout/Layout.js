3369cfedd0451b055fd1f37f331ebec74578ea6a
Aymeric TOULOUSE <atoulous@e1r2p17.42.fr>
2018-06-10 22:36:29 +0200

    sun10jun - add user menu on app bar

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
@@ -8,8 +8,12 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
-import Menu from '../Menu';
+import MenuData from '../Menu';
 
 const styles = theme => ({
   root: {
@@ -27,6 +31,9 @@ const styles = theme => ({
     position: 'relative',
     width: 240,
   },
+  flex: {
+    flex: 1,
+  },
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
@@ -38,29 +45,91 @@ const styles = theme => ({
     color: 'white',
     textDecoration: 'none',
   },
+  menuUser: {
+    marginLeft: 'auto',
+  },
 });
 
-const Layout = ({ classes, children }) => (
-  <div className={classes.root}>
-    <AppBar position="absolute" className={classes.appBar}>
-      <Toolbar>
-        <Link to="/" title="Home" className={classes.homeButton}>
-          <Typography variant="title" color="inherit" noWrap>Hypertube</Typography>
-        </Link>
-      </Toolbar>
-    </AppBar>
-    <Drawer variant="permanent" classes={{ paper: classes.drawerPaper }}>
-      <div className={classes.toolbar} />
-      <List>
-        <Menu />
-      </List>
-    </Drawer>
-    <main className={classes.content}>
-      <div className={classes.toolbar} />
-      {children}
-    </main>
-  </div>
-);
+class Layout extends Component {
+  state = {
+    auth: true,
+    anchorEl: null,
+  };
+
+  handleMenu = (event) => {
+    console.log('handleMenu');
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    console.log('handleClose');
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { classes, children } = this.props;
+    const { auth, anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="absolute" className={classes.appBar}>
+          <Toolbar>
+            <Link to="/" title="Home" className={classes.homeButton}>
+              <Typography
+                variant="title"
+                color="inherit"
+                className={classes.flex}
+              >Hypertube
+              </Typography>
+            </Link>
+
+            {auth && (
+            <div className={classes.menuUser}>
+              <IconButton
+                aria-owns={open ? 'menu-appbar' : null}
+                aria-haspopup="true"
+                onClick={this.handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={this.handleClose}
+              >
+                <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                <MenuItem onClick={this.handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+            )}
+
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" classes={{ paper: classes.drawerPaper }}>
+          <div className={classes.toolbar} />
+          <List>
+            <MenuData />
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          {children}
+        </main>
+      </div>
+    );
+  }
+}
 
 Layout.propTypes = {
   classes: PropTypes.object.isRequired,
