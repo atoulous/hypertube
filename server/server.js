8148b15173bbb3a508896a420e34f40f19580d04
Martin STEFFEN <mail@tiph.io>
2018-06-13 11:53:06 +0200

    Added request logging

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
-
+import morgan from 'morgan'
 import config from './config';
 import mediaRoutes from './routes/media'
+
 
 const app = express();
 
@@ -9,6 +10,6 @@ app.use('/', mediaRoutes)
 
 // Serve streams files
 app.use(express.static('controllers/streams'))
-
+app.use(morgan('combined'))
 
 app.listen(config.port, () => console.log(`Listening on port ${config.port}`));
