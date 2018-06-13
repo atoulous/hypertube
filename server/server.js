a55384656c6b4b6b1ce62e0a4959a146f5749c43
Martin STEFFEN <mail@tiph.io>
2018-06-13 11:54:41 +0200

    Chaged logging rule

--- server/server.js
+++ server/server.js
@@ -3,13 +3,12 @@ import morgan from 'morgan'
 import config from './config';
 import mediaRoutes from './routes/media'
 
-
 const app = express();
+app.use(morgan('dev'))
 
 app.use('/', mediaRoutes)
 
 // Serve streams files
 app.use(express.static('controllers/streams'))
-app.use(morgan('combined'))
 
 app.listen(config.port, () => console.log(`Listening on port ${config.port}`));
