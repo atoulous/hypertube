ab97f1cd8cccac6b3873b88bb5ff59be1e50be5b
Martin STEFFEN <mail@tiph.io>
2018-06-13 11:55:07 +0200

    Removed express.static

--- server/server.js
+++ server/server.js
@@ -6,11 +6,10 @@ import mediaRoutes from './routes/media'
 const app = express();
 app.use(morgan('dev'))
 
-app.use('/', mediaRoutes)
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/hypertube')
 
-// Serve streams files
-app.use(express.static('controllers/streams'))
+
+app.use('/', mediaRoutes)
 
 app.listen(config.port, () => console.log(`Listening on port ${config.port}`));
