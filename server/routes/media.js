c214e458a5dae923215c2f24b60476f0615d9e75
Martin STEFFEN <mail@tiph.io>
2018-06-13 19:16:33 +0200

    Improved streaming quality, latency, ...

--- server/routes/media.js
+++ server/routes/media.js
@@ -3,6 +3,7 @@
 const router = require('express').Router()
 const Media = require('../models/Media')
 const MediaController = require('../controllers/MediaController')
+const fs = require('fs')
 
 router.get('/media', (req, res) => {
 	Media.find({}).then(medias => {
@@ -26,6 +27,28 @@ router.get('/startmedia/:id', async (req, res) => {
 	}).catch(err => {
 		return res.status(404).end('that does not exist')
 	})
+})
+
+
+const serveWhenAvailable = async (res, filePath) => {
+	if (fs.existsSync(filePath)) {
+		return res.status(200).end(fs.readFileSync(filePath))
+	} else {
+		console.log(filePath, 'does not exist.')
+		setTimeout(() => {
+			return serveWhenAvailable(res, filePath)
+		}, 1000)
+	}
+}
+router.get('/:mid/*.m3u8', (req, res) => {
+	res.set('Content-type', 'application/x-mpegURL')
+	const filePath = __dirname + '/../controllers/streams/' + req.params.mid + '/ps.m3u8'
+	serveWhenAvailable(res, filePath)
+})
+router.get('/:mid/*.ts', (req, res) => {
+	const filePath = __dirname + '/../controllers/streams' + req.originalUrl
+	res.set('Content-type', 'application/octet-stream')
+	serveWhenAvailable(res, filePath)
 })
 
 router.get('/play/:id', (req, res) => {
