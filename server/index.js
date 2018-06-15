1856a6cd43a95aa08c8125d1a30b24b196b99c3a
Martin STEFFEN <mail@tiph.io>
2018-06-15 14:00:35 +0200

    Added CORS headers

--- server/index.js
+++ server/index.js
@@ -8,6 +8,12 @@ import crawler from './crawler/crawler';
 
 const app = express();
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 mongoose.connect(config.db.url);
 
 crawler.startCrawling();
