a04e86600f9aa9836702142619e01822dbd14388
Martin STEFFEN <mail@tiph.io>
2018-06-23 13:32:44 +0200

    Fixed crawler parameters

--- server/crawler/crawler.js
+++ server/crawler/crawler.js
@@ -5,10 +5,10 @@ const ThePirateBay = require('./ThePirateBay'),
 const crawl = async () => {
   try {
     Promise.resolve([
-      ThePirateBay.crawl('/top/201',			'movie', 	-1, true, 'movie'),
-      ThePirateBay.crawl('/top/205',			'show',		-1, true, 'tv'),
-      Crawler1337x.crawl('/trending/w/tv/', 	'show', 	-1, true, 'tv'),
-      Crawler1337x.crawl('/trending/w/movies/', 'movie', 	-1, true, 'movie')
+      ThePirateBay.crawl('/top/201',			'movie', 	-1, 'movie'),
+      ThePirateBay.crawl('/top/205',			'show',		-1, 'tv'),
+      Crawler1337x.crawl('/trending/w/tv/', 	'show', 	-1, 'tv'),
+      Crawler1337x.crawl('/trending/w/movies/', 'movie', 	-1, 'movie')
     ]);
   } catch (e) {
     console.log(e);
@@ -17,10 +17,10 @@ const crawl = async () => {
 
 const search = async (searchTerm) => {
 	const results = await Promise.all([
-		ThePirateBay.crawl(`/search/${searchTerm}/0/99/201`, 			'movie', 	2, false, 'movie'),
-		ThePirateBay.crawl(`/search/${searchTerm}/0/99/205`, 			'show', 	2, false, 'tv'),
-		Crawler1337x.crawl(`/category-search/${searchTerm}/Movies/1/`, 	'movie', 	2, false, 'movie'),
-		Crawler1337x.crawl(`/category-search/${searchTerm}/TV/1/`, 		'show', 	2, false, 'tv')
+		ThePirateBay.crawl(`/search/${searchTerm}/0/99/201`, 			'movie', 	2, 'movie'),
+		ThePirateBay.crawl(`/search/${searchTerm}/0/99/205`, 			'show', 	2, 'tv'),
+		Crawler1337x.crawl(`/category-search/${searchTerm}/Movies/1/`, 	'movie', 	2, 'movie'),
+		Crawler1337x.crawl(`/category-search/${searchTerm}/TV/1/`, 		'show', 	2, 'tv')
 	])
 
 	return results
